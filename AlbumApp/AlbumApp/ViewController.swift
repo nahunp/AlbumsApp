92d143cbf567e083e8473bb6775392fa21397cf6
nahunp <nahun.perezv@gmail.com>
2025-05-09 12:18:17 -0400

    first commit

---
+++ AlbumApp/AlbumApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumApp
+//
+//  Created by CincinnatiAi Dallas on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
