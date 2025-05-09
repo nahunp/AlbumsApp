acc266c548be5e721606cdc5a15f219d10aff687
nahunp <nahun.perezv@gmail.com>
2025-05-09 16:54:58 -0400

    Interview Questions and AlbumApp

---
+++ AlbumApp/AlbumApp/Model/Albums.swift
@@ -0,0 +1,23 @@
+//
+//  Album.swift
+//  AlbumApp
+//
+//  Created by CincinnatiAi Dallas on 5/9/25.
+//
+
+import Foundation
+struct Album {
+    let albumId: Int
+    let id: Int
+    let title: String
+    let url: String
+    let thumbnailUrl: String
+}
+
+//{
+//        "albumId": 1,
+//        "id": 1,
+//        "title": "accusamus beatae ad facilis cum similique qui sunt",
+//        "url": "https://via.placeholder.com/600/92c952",
+//        "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+//    }
---
+++ AlbumApp/AlbumApp/Model/Books.swift
@@ -0,0 +1,14 @@
+//
+//  Books.swift
+//  AlbumApp
+//
+//  Created by CincinnatiAi Dallas on 5/9/25.
+//
+
+import Foundation
+
+struct Book {
+    let title: String
+    let author: String
+}
+
